5054b162ae1b836dc05ac86fc03446aebedaea79
Niamh Power <niamhpower@live.com>
2017-11-28 17:09:57 +0000

    created blank activity in Kotlin 🙌🏻

---
+++ app/src/main/java/com/niamhpower/fay/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.niamhpower.fay
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
